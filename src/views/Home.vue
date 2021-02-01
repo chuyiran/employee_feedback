708262ee4efabb3907d65c8706d5e73a9775a603
储怡然 <252964202@qq.com>
2021-02-01 23:04:56 +0800

    second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box">这只是一个div标签</div>
   </div>
 </template>
 
@@ -16,3 +23,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.box{
+  font-size: 20px;
+}
+</style>
